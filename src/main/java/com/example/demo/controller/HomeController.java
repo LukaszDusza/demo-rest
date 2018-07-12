b90c4f850f2490009764185de62e4e7586cce595
LukaszDusza <lukaszdusza280@gmail.com>
2018-07-12 23:28:10 +0200

    12-07-2018

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+   // @RequestMapping(value = "home", method = RequestMethod.GET)
+    public String home() {
+        return "index";
+    }
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+    @RequestMapping(value = "products", method = RequestMethod.GET)
+    public String messages(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "products";
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.Repository.ProductRepository;
+import com.example.demo.entities.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/")
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping("products")
+    public List<Product> products() {
+        return productRepository.findAll();
+    }
+
+    @RequestMapping(value = "products", method = RequestMethod.POST)
+    public ResponseEntity<Product> addBook(@Valid @RequestBody Product product) {
+        productRepository.save(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+
+}
